ad39452753d85ec3686d8e3bf40bb74608bf4b11
Phuoc .Le <phuoc.le@tekcent.com>
2020-05-29 01:42:51 +0700

    update code

--- src/app/src/main/java/hcmute/edu/vn/nhom02/foodyproject/RestaurantDetail.java
+++ src/app/src/main/java/hcmute/edu/vn/nhom02/foodyproject/RestaurantDetail.java
@@ -3,12 +3,19 @@ package hcmute.edu.vn.nhom02.foodyproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class RestaurantDetail extends AppCompatActivity {
 
+    TextView tvDistance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+
+        tvDistance = (TextView) findViewById(R.id.tvDistance);
+        String text = "<font color=#00ff00>4.2km</font>Second Color";
+        tvDistance.setText(Html.fromHtml(text));
     }
 }
